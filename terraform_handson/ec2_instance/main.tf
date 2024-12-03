b6f75206ba5818bcab97bbf342a5176b80cb5987
Peter Ngoh <ngohpeter50@yahoo.com>
2024-12-03 16:30:18 +0100

    Write terraform script to launch ec2 instance with security group attached

---
+++ terraform_handson/ec2_instance/main.tf
@@ -0,0 +1,12 @@
+# defining the EC2 Instance
+
+resource "aws_instance" "test_ec2" {
+  ami = "ami-0b5673b5f6e8f7fa7" # amazon linux
+  instance_type = "t2.micro"
+  key_name = "demo_key"
+  security_groups = [aws_security_group.allow_ssh.name]
+  tags = {
+    Name: "Via_Terraform"
+  }
+
+}
