4f9adbabb55e09fdf76ca64809d0fa702bef8ea0
Peter Ngoh <ngohpeter50@yahoo.com>
2024-12-06 21:52:13 +0100

    add security group id

--- terraform_handson/ec2_instance/main.tf
+++ terraform_handson/ec2_instance/main.tf
@@ -4,7 +4,7 @@ resource "aws_instance" "test_ec2" {
   ami             = "ami-0b5673b5f6e8f7fa7" # amazon linux
   instance_type   = "t2.micro"
   key_name        = "demo_key"
-  security_groups = [aws_security_group.allow_ssh.name]
+  security_groups = [aws_security_group.allow_ssh.id]
   tags = {
     Name : "Via_Terraform"
   }
