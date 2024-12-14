cc55464a284847f8ca94cb2dd4c37c5ccd6b58f2
Peter Ngoh <ngohpeter50@yahoo.com>
2024-12-14 16:47:00 +0100

    added terraform document with explanation of common terms

--- terraform_handson/module/ec2/output.tf
+++ terraform_handson/module/ec2/output.tf
@@ -7,3 +7,7 @@ output "instance_id" {
   value       = aws_instance.ec2_instance.id
   description = "The ID of the EC2 instance"
 }
+
+output "private_ip" {
+  value = aws_instance.ec2_instance.private_ip
+}
