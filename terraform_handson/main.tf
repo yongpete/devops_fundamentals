4ff39b0814b1fa7630d890d502b8dd6a0b5dc37a
Peter Ngoh <ngohpeter50@yahoo.com>
2024-12-14 22:58:26 +0100

    refactored iam role file to remove hard coded policies

--- terraform_handson/main.tf
+++ terraform_handson/main.tf
@@ -1,8 +1,21 @@
 module "ec2_module" {
-  source = "./module/ec2"
-  instance_name = var.instance_name
-  role_name = var.role_name
-  policy_name = var.policy_name
-  instance_type = var.instance_type
+  source                = "./module/ec2"
+  instance_name         = var.instance_name
+  role_name             = var.role_name
+  policy_name           = var.policy_name
+  instance_type         = var.instance_type
   instance_profile_name = var.instance_profile_name
+  aws_iam_role_policy   = file("s3-policy.json")
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Effect = "Allow",
+        Principal = {
+          Service = "ec2.amazonaws.com"
+        },
+        Action = "sts:AssumeRole"
+      }
+    ]
+  })
 }
